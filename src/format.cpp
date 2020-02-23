a7a732dd697834913115a8c90eddd9995aad1b97
chaliburton <chris.haliburton@gmail.com>
2020-02-22 22:54:06 -0500

    Populated helper function

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,13 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: this helper function converts seconds in long int format to HH:MM:SS string format 
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// DONE: REMOVE: [[maybe_unused]] once you define the function
+string Format::ElapsedTime(long seconds) { 
+    string HH{std::to_string(seconds/60/60)};               // revisit, may need to use floor cmath function 
+    string MM{std::to_string((seconds/60)%60)};
+    string SS{std::to_string(seconds%60)};
+    return HH + ":" + MM + ":" + SS;
+}
